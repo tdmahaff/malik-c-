6ceca0b303c9e36d210eb239b2a8f45af6a9a0e2
tdmahaff <151571455+tdmahaff@users.noreply.github.com>
2026-09-11 16:47:38 -0600

    Add files via upload

--- 2_10.cpp
+++ 2_10.cpp
@@ -1,20 +1,22 @@
-/* Write a program that prompts the user to input five decimal numbers.
-The program should then add the five decimal numbers, convert the
-sum to the nearest integer, and print the result*/
+//
+//  Write a program that prompts the user to input five decimal numbers.
+// The program should then add the five decimal numbers, convert the
+// sum to the nearest integer, and print the result.
+//
 
 #include <iostream>
 using namespace std;
 
+
 int main() {
-
-	double num1, num2, num3, num4, num5, sum;
-	cout << "Input five decimal numbers: ";
-	cin >> num1 >> num2 >> num3 >> num4 >> num5;
-
-	sum = num1 + num2 + num3 + num4 + num5;
-
-	cout << "Sum converted the nearest integer is: " << static_cast<int>(sum + 0.5);
-
-	return 0;
-
+    
+    double num1 = 0, num2 = 0, num3 = 0, num4 = 0, num5 = 0;
+    
+    cout << "Enter five numbers separated by a space: ";
+    cin >> num1 >> num2 >> num3 >> num4 >> num5;
+    
+    cout << static_cast<int>(num1 + num2 + num3 + num4 + num5 + .5);
+    
+    return 0;
+    
 }
